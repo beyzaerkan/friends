0dfec0ab23d4b5a408487d63a9e8d0d7aa8f9302
beyzaerkan <beyzaerkan@icloud.com>
2022-07-23 21:35:50 +0300

    card component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import Card from './components/Card'
 
 function App() {
+  const codes = [
+    {
+      statusCode: 101,
+      title: "Switching",
+    },
+    {
+      statusCode: 102,
+      title: "Processing",
+    },
+  ]
+
+
   return (
     <div className="App">
       <h1>Friends</h1>
+      <div className='cards'>
+      {
+        codes.map(item => {
+          return (
+            <Card key={item} imageUrl={require("../images/" + item.statusCode + ".jpg")} statusCode={item.statusCode} title={item.title}></Card>
+          )
+        })
+      }
+      </div>
     </div>
   );
 }
